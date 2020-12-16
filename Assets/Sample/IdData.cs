d71ec5a175f3f96d4301e38a6b447767c9615a2c
NicatorBa <daniel@bartschinski.info>
2020-12-16 20:49:11 +0100

    Initial commit

---
+++ Assets/Sample/IdData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Mirror;
+
+namespace Sample
+{
+    public readonly struct IdData
+    {
+        private readonly string _id;
+
+        private IdData(string id)
+        {
+            _id = id;
+        }
+
+        public static IdData Parse(string id)
+        {
+            return new IdData(id);
+        }
+
+        public override string ToString()
+        {
+            return _id;
+        }
+    }
+    
+    public static class IdDataReaderWriter
+    {
+        public static void WriteIdData(this NetworkWriter writer, IdData data)
+        {
+            writer.WriteString(data.ToString());
+        }
+     
+        public static IdData ReadIdData(this NetworkReader reader)
+        {
+            return IdData.Parse(reader.ReadString());
+        }
+    }
+}
---
+++ Assets/Sample/Transmitter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using Sample;
+using UnityEngine;
+
+public class Transmitter : NetworkBehaviour
+{
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        StartCoroutine(SendIds());
+    }
+
+    private IEnumerator SendIds()
+    {
+        var i = 0;
+        while (i < 10)
+        {
+            RpcReceiveId(IdData.Parse(Guid.NewGuid().ToString()));
+            i++;
+
+            yield return new WaitForSeconds(10);
+        }
+    }
+
+    [ClientRpc]
+    private void RpcReceiveId(IdData data)
+    {
+        Debug.Log(data);
+    }
+}
